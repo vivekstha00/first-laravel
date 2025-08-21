11e87c4ebfaffc882ba11b20bc4dc2eba3a288d4
bbbekstha <bs2288175@gmail.com>
2025-08-21 10:15:03 +0545

    Dashboard Updated

--- resources/views/admin/page/dashboard.blade.php
+++ resources/views/admin/page/dashboard.blade.php
@@ -3,31 +3,31 @@
 @section('admin-content')
 <body>
     <div class="col-md-9 ms-sm-auto col-lg-10 px-md-4" style="margin-left: 16.666667%;">
-        <div class="d-flex justify-content-between align-items-center mb-3">
+        <div class="mb-4">
             <h1>Dashboard</h1>
-            <div class="row mt-4">
-                <div class="col-md-3">
-                    <div class="card text-white bg-primary mb-3 shadow">
-                        <div class="card-body">
-                            <h5 class="card-title">Total Users</h5>
-                            <p class="card-text fs-3 fw-bold">{{ $statistics['totalUsersCount'] }}</p>
-                        </div>
+        </div>
+        <div class="row">
+            <!-- Total Users -->
+            <div class="col-md-6 mb-3">
+                <div class="card border-0 shadow-sm bg-primary text-white">
+                    <div class="card-body d-flex justify-content-between align-items-center">
+                        <h5 class="mb-0">Total Users</h5>
+                        <h3 class="text-white fw-bold mb-0">{{ $statistics['totalUsersCount'] }}</h3>
                     </div>
                 </div>
-
-                <div class="col-md-3">
-                    <div class="card text-white bg-secondary mb-3 shadow">
-                        <div class="card-body">
-                            <h5 class="card-title">Total Admin</h5>
-                            <p class="card-text fs-3 fw-bold">{{ $statistics['totalAdminsCount'] }}</p>
-                        </div>
+            </div>
+            
+            <!-- Total Admin -->
+            <div class="col-md-6 mb-3">
+                <div class="card border-0 shadow-sm bg-success text-white">
+                    <div class="card-body d-flex justify-content-between align-items-center">
+                        <h5 class="mb-0">Total Admin</h5>
+                        <h3 class="text-white fw-bold mb-0">{{ $statistics['totalAdminsCount'] }}</h3>
                     </div>
                 </div>
             </div>
         </div>
-      
-    </div>
 
+    </div>
 </body>
-   
 @endsection
